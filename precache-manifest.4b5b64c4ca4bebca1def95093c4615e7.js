4ce4a85afcf517a06a547e8d043b78ed2bc97417
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-06-11 17:40:27 -0500

    Updates

---
+++ precache-manifest.4b5b64c4ca4bebca1def95093c4615e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1c9f8e9b0389c580d778861a6a37d9",
+    "url": "/monstercards-reactjs/index.html"
+  },
+  {
+    "revision": "4babdad68fea8fec4dc4",
+    "url": "/monstercards-reactjs/static/css/main.45f9d8e4.chunk.css"
+  },
+  {
+    "revision": "f40393fb71354efe8bad",
+    "url": "/monstercards-reactjs/static/js/2.073af374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstercards-reactjs/static/js/2.073af374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4babdad68fea8fec4dc4",
+    "url": "/monstercards-reactjs/static/js/main.502aac49.chunk.js"
+  },
+  {
+    "revision": "ef27b8d2896d60fb8065",
+    "url": "/monstercards-reactjs/static/js/runtime-main.0996acd3.js"
+  }
+]);
