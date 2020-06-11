c827f70ff670d8652747a766dff824cc8cb3e471
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-06-11 17:59:35 -0500

    Updates

---
+++ precache-manifest.abcfe89f453eaa0ca1d58dc6eceacdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3fc09465d732aa76aaa0faf74c266b",
+    "url": "/MonsterCards-ReactJs/index.html"
+  },
+  {
+    "revision": "b687d994592ab0be9a4a",
+    "url": "/MonsterCards-ReactJs/static/css/main.45f9d8e4.chunk.css"
+  },
+  {
+    "revision": "f40393fb71354efe8bad",
+    "url": "/MonsterCards-ReactJs/static/js/2.073af374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterCards-ReactJs/static/js/2.073af374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b687d994592ab0be9a4a",
+    "url": "/MonsterCards-ReactJs/static/js/main.af5839d0.chunk.js"
+  },
+  {
+    "revision": "c04d6dac3aab0d8228ca",
+    "url": "/MonsterCards-ReactJs/static/js/runtime-main.026d5973.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monstercards-reactjs/precache-manifest.4b5b64c4ca4bebca1def95093c4615e7.js"
+  "/MonsterCards-ReactJs/precache-manifest.abcfe89f453eaa0ca1d58dc6eceacdd8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monstercards-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MonsterCards-ReactJs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
